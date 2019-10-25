5c0d26f435b3ab67e4d80b2d7057eb00662680de
Bogdan Maizel <bogdan.maizel@student.utcluj.ro>
2019-10-25 13:44:24 +0300

    trying to push to dev branch

--- carshop/main.c
+++ carshop/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
-    printf("Hello, car enthusiast!\n");
+    printf("Welcome to our car shop!\n");
+    // Input personal data
+    char firstName[20];
+    char lastName[20];
+    int phoneNumber[10];
+    char address[50];
+    printf("Please input your data\n");
+    printf("First name: ");
+    gets(firstName);
+    printf("Last name: ");
+    gets(lastName);
+    printf("Phone number(ONLY DIGITS):\n");
+    scanf("%d", phoneNumber);
+    printf("Address:\n");
+    gets(address);
+    gets(address);
     return 0;
 }
